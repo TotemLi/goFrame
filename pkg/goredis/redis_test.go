acca99f1edabd8be9f6cc40709642bd961f82a32
TotemLi <1027029629@qq.com>
2023-02-09 16:47:09 +0800

    small

--- pkg/goredis/redis_test.go
+++ pkg/goredis/redis_test.go
@@ -2,41 +2,23 @@ package goredis
 
 import (
 	"context"
-	"fmt"
 	"testing"
 )
 
 func TestNewClient(t *testing.T) {
 	c := NewClient("localhost:6379", "MQgnbBmJ3tsjiiR4", 0)
 	ctx := context.Background()
-	var s bool
-	err := c.Get(ctx, "totemli", &s)
-	if err != nil {
-		if err == ErrNotFind {
-			fmt.Println("not find")
-			return
-		}
-		fmt.Println("err: ", err)
-		return
+	type Student struct {
+		Name string `json:"name"`
+		Age  int    `json:"age"`
 	}
-	fmt.Println("s: ", s)
-	//d := struct {
-	//	A string
-	//	B int
-	//}{
-	//	A: "a",
-	//	B: 123,
-	//}
-	//v := map[string]any{
-	//	"a": "a",
-	//	"b": 123,
-	//}
-	//v := "abc"
-	//v := []byte("abc")
-	//v, e := json.Marshal(12.345)
-	//
-	//t.Log(string(v))
-	//t.Log("err: ", e)
-	//err := c.Set(ctx, "a", v)
-	//t.Log("err: ", err)
+	stu := Student{
+		Name: "TotemLi",
+		Age:  24,
+	}
+	c.Set(ctx, "totemli", stu)
+	var stu2 Student
+	err := c.Get(ctx, "totemli", &stu2)
+	t.Log("err: ", err)
+	t.Log("stu2: ", stu2)
 }
